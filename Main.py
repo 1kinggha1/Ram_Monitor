47d6b0ee3b213c7e5302aa3f917f0926a1c876d0
1kinggha1 <100527791+1kinggha1@users.noreply.github.com>
2024-10-25 22:02:00 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,13 +2,19 @@
 $python -m uvicorn Main:Ram_Api --reload
 to run the main program and activate the API
 """
-#Need the fastApi to make the API
+#Need the fastApi to make the api
 from fastapi import FastAPI
+
+#to start recording the data 
+#from subprocess import Popen,run
 
 #bring sqlite3 to the app
 import sqlite3
-#create the API and the get method of it
+#create the api and the get method of it
 Ram_Api= FastAPI()
+@Ram_Api.get("/")
+def null_adress():
+    return "please add /n at the end of the Url (n being the number of the records)"
 @Ram_Api.get("/{item_id}")
 def present(item_id :int = 1):
     data=sqlite3.connect("DataBase.db")
@@ -25,7 +31,15 @@ def present(item_id :int = 1):
         for i in range(len(js)):
             js[i]=dict(zip(l,one_set[i]))
     except:
-        one_set="internal server error"
+        #error handling
+        curs.execute("SELECT COUNT(*) FROM system_ram")
+        count = curs.fetchone()[0]
+        if item_id>count:
+            # only show available data
+            js.insert(0,f"we only have {count} record:")
+        else:
+            #unknown error
+            js="There might be an internal error"
     finally:
-        return one_set
-        curs.close()
+            #finally, send out the data
+        return js
